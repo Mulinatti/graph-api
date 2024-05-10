64e006e2a4af2733b7a6e7b1eed554005e3fcecd
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-10 11:21:58 -0300

    Inclusão da classe abstrata GrafoAbstrato, que consiste nos dados comuns aos dois tipos de grafos: grafo e digrafo.
    Duas classes modeladas: Grafo e Digrafo. A primeira trata de um grafo não direcionado, enquanto a segunda de um grafo direcionado.

--- grafo.py
+++ grafo.py
@@ -1,17 +1,34 @@
+from abc import ABC, abstractmethod
 from typing import List
+from dataclasses import dataclass
 
-class Grafo:
+class GrafoAbstrato(ABC):
+    """Classe abstrata com todos as funcionalidades comuns aos
+       dois tipos de grafos modelados: grafo e digrafo (grafo
+       dirigido)"""
+    def __init__(self, mat_adj: List[List[int]]):
+        self.mat_adj = mat_adj
+        self.num_vertices = len(mat_adj)
+        self.num_arestas = self.contar_arestas(mat_adj)
+        self.mat_incid = self.adj_para_incid(mat_adj)
+    
+    @abstractmethod
+    def contar_arestas(self, adjacencias: List[List[int]]):
+        pass
+
+    @abstractmethod
+    def adj_para_incid(self, adjacencias: List[List[int]]):
+        pass
+
+
+class Grafo(GrafoAbstrato):
     """Classe que encapsula todos os dados necessários de um grafo.
        Possui as matrizes de adjacências e de incidência, o número
        de vértices e de arestas, e uma lista com o grau de cada
        vértice."""
-    def __init__(self, mat_adj: List[List[int]], direcionado: bool):
-        self.mat_adj        = mat_adj
-        self.mat_incid      = self.adj_para_incid(mat_adj)
-        self.num_vertices   = len(mat_adj)
-        self.num_arestas    = self.contar_arestas(mat_adj)
-        self.lista_de_graus = self.calcular_grau(mat_adj)
-        self.direcionado    = direcionado
+    def __init__(self, mat_adj: List[List[int]]):
+        super().__init__(mat_adj)
+        self.lista_de_graus = self.calcular_graus(mat_adj)
     
     def adj_para_incid(self, adjacencias) -> List[List[int]]:
         # Populando a nova matriz com zeros
@@ -20,17 +37,6 @@ class Grafo:
             incidencia.append([0] * self.num_arestas)
         
         indice_aresta = 0
-        # Algoritmo diferente dependendo de ser um grafo direcionado ou não.
-        if self.direcionado:
-            for i in range(self.num_vertices):
-                for j in range(i+1, self.num_vertices):
-                    if adjacencias[j][i] != 0:
-                        incidencia[i][indice_aresta] += adjacencias[i][j]
-                        incidencia[j][indice_aresta] += adjacencias[j][i]
-                        indice_aresta += 1
-            return incidencia
-        
-        # Não direcionado:
         for i in range(self.num_vertices):
             # Laços
             if adjacencias[i][i] != 0:
@@ -47,6 +53,40 @@ class Grafo:
     def contar_arestas(self, adjacencias: List[List[int]]) -> int:
         pass
 
-    def calcular_grau(self, adjacencias: List[List[int]]) -> List[int]:
+    def calcular_graus(self, adjacencias: List[List[int]]) -> List[int]:
         pass
 
+
+@dataclass
+class Vertice:
+    """Classe de dados necessária para armazenar
+       os graus de entrada e de saída de um vértice."""
+    grau_entrada: int
+    grau_saida: int
+    grau_total: int
+
+
+class Digrafo(GrafoAbstrato):
+    """Classe que encapsula os dados necessário para modelar
+       um grafo direcionado."""
+    def __init__(self, mat_adj: List[List[int]]):
+        super().__init__(mat_adj)
+        self.lista_graus = self.calcular_graus(mat_adj)
+    
+    def adj_para_incid(self, adjacencias: List[List[int]]):
+        # Zerando a matriz de incidência.
+        incidencia = []
+        incidencia = [[0] * self.num_arestas for _ in range(self.num_vertices)]
+        
+        indice_aresta = 0
+        for i in range(self.num_vertices):
+            for j in range(i+1, self.num_vertices):
+                if adjacencias[i][j] != 0:
+                    incidencia[i][indice_aresta] += adjacencias[i][j]
+                    incidencia[j][indice_aresta] -= adjacencias[i][j]
+                    indice_aresta += 1
+        
+        return incidencia
+    
+    def calcular_graus(self, adjacencias: List[List[int]]):
+        pass
