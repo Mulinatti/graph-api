41c7bc0d3a3db240ecffbf4928d78ba912809e80
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-13 15:55:04 -0300

    Começo da implementação dos testes unitários.

--- Functions/contar_arestas.py
+++ Functions/contar_arestas.py
@@ -1,9 +1,23 @@
-from typing import List
+import unittest
 
-def contar_arestas(adjacencias: List[List[int]]) -> int:
+def contar_arestas(adjacencias: list[list[int]]) -> int:
         num_arestas = 0
         for i in range(len(adjacencias)):
             for j in range(i, len(adjacencias)):
                 if i <= j:
                     num_arestas += adjacencias[i][j]
         return num_arestas
+
+
+class TestContarArestas(unittest.TestCase):
+     def test_1(self):
+          self.assertEqual(contar_arestas([
+               [0, 1, 0, 0],
+               [1, 0, 1, 0],
+               [0, 1, 0, 1],
+               [0, 0, 1, 0]
+          ]), 3)
+
+
+if __name__ == '__main__':
+     unittest.main(verbosity=2)
