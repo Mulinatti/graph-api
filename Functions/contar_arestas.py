5628b915e6db107d1c4083640d87405a805677bc
JoaoMathus <joaomatheusGDO@gmail.com>
2024-05-14 08:34:42 -0300

    Pequena modificação no algoritmo contar_arestas, e organização dos seus parâmetros para serem consistentes com a ordem das outras funções.

--- Functions/contar_arestas.py
+++ Functions/contar_arestas.py
@@ -1,12 +1,12 @@
 import unittest
 
-def contar_arestas(adjacencias, valorado: bool, digrafo : bool) -> int:
+def contar_arestas(adjacencias, digrafo: bool, valorado: bool) -> int:
      num_arestas = 0
      if valorado:
           for i in range(len(adjacencias)):
                for j in range(i, len(adjacencias)):
-                    if i <= j:  
-                         num_arestas += adjacencias[i][j]
+                    if adjacencias[i][j] > 0:  
+                         num_arestas += 1
           return num_arestas
      elif digrafo:
           for i in range(len(adjacencias)):
@@ -20,89 +20,99 @@ def contar_arestas(adjacencias, valorado: bool, digrafo : bool) -> int:
                          num_arestas += adjacencias[i][j]
           return num_arestas
 
+
 class TestContarArestas(unittest.TestCase):
-    def test_1(self):
-        """Testar um grafo bem básico mesmo."""
-        self.assertEqual(contar_arestas([
-            [0, 1, 0, 0],
-            [1, 0, 1, 0],
-            [0, 1, 0, 1],
-            [0, 0, 1, 0]
-        ],valorado=False, digrafo=False), 3)
+     def test_1(self):
+          """Testar um grafo bem básico mesmo."""
+          self.assertEqual(contar_arestas([
+               [0, 1, 0, 0],
+               [1, 0, 1, 0],
+               [0, 1, 0, 1],
+               [0, 0, 1, 0]
+          ], False, False), 3)
+     
+     def test_2(self):
+          """Testar um grafo não valorado com laço."""
+          self.assertEqual(contar_arestas([
+               [1, 1, 0],
+               [1, 0, 1],
+               [0, 1, 1]
+          ], False, False), 4)
+     
+     def test_3(self):
+          """Testar um grafo com arestas múltiplas."""
+          self.assertEqual(contar_arestas([
+               [0, 2, 2],
+               [2, 0, 2],
+               [2, 2, 0]
+          ], False, False), 6)
+     
+     def test_4(self):
+          """Testar um grafo valorado."""
+          self.assertEqual(contar_arestas([
+               [0, 20, 5],
+               [20, 0, 15],
+               [5, 15, 0]
+          ], False, True), 3)
+     
+     def test_5(self):
+          """Testar um grafo valorado e com laço."""
+          self.assertEqual(contar_arestas([
+               [15, 3, 2],
+               [3, 0, 0],
+               [2, 0, 0]
+          ], False, True), 3)
+     
+     def test_6(self):
+          """Testar um digrafo bem simples mesmo."""
+          self.assertEqual(contar_arestas([
+               [0, 1, 1],
+               [0, 0, 0],
+               [0, 0, 0]
+          ], True, False), 2)
+     
+     def test_7(self):
+          """Testar um digrafo com laço."""
+          self.assertEqual(contar_arestas([
+               [1, 1, 0, 0],
+               [0, 0, 0, 0],
+               [0, 1, 0, 0],
+               [0, 0, 1, 0]
+          ], True, False), 4)
+     
+     def test_8(self):
+          """Testar um digrafo com aresta dupla."""
+          self.assertEqual(contar_arestas([
+               [0, 1, 0],
+               [1, 0, 0],
+               [0, 0, 0]
+          ], True, False), 2)
+     
+     def test_9(self):
+          """Testar um digrafo com múltiplas arestas."""
+          self.assertEqual(contar_arestas([
+               [0, 1, 0, 0],
+               [1, 0, 1, 0],
+               [0, 1, 0, 2],
+               [0, 0, 2, 0]
+          ], True, False), 8)
+     
+     def test_10(self):
+          """Testar um digrafo com laço e arestas duplas."""
+          self.assertEqual(contar_arestas([
+               [0, 0, 0],
+               [0, 1, 1],
+               [2, 1, 0]
+          ], True, False), 5)
+    
+     def test_11(self):
+         """Testar um digrafo valorado."""
+         self.assertEqual(contar_arestas([
+             [0, 5, 0],
+             [0, 0, 10],
+             [0, 0, 0]
+         ], True, True), 2)
 
-    def test_2(self):
-        """Testar um grafo não valorado com laço."""
-        self.assertEqual(contar_arestas([
-            [1, 1, 0],
-            [1, 0, 1],
-            [0, 1, 1]
-        ],valorado=False, digrafo=False), 4)
-
-    def test_3(self):
-        """Testar um grafo com arestas múltiplas."""
-        self.assertEqual(contar_arestas([
-            [0, 2, 2],
-            [2, 0, 2],
-            [2, 2, 0]
-        ],valorado=False, digrafo=False), 6)
-
-    def test_4(self):
-        """Testar um grafo valorado."""
-        self.assertEqual(contar_arestas([
-            [0, 20, 5],
-            [20, 0, 15],
-            [5, 15, 0]
-        ],valorado=True, digrafo=False), 40)
-
-    def test_5(self):
-        """Testar um grafo valorado e com laço."""
-        self.assertEqual(contar_arestas([
-            [15, 3, 2],
-            [3, 0, 0],
-            [2, 0, 0]
-        ], valorado=True, digrafo=False), 23)
-
-    def test_6(self):
-        """Testar um digrafo bem simples mesmo."""
-        self.assertEqual(contar_arestas([
-            [0, 1, 1],
-            [0, 0, 0],
-            [0, 0, 0]
-        ],valorado=False, digrafo=False), 2)
-
-    def test_7(self):
-        """Testar um digrafo com laço."""
-        self.assertEqual(contar_arestas([
-            [1, 1, 0, 0],
-            [0, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0]
-        ],valorado=False, digrafo=True), 4)
-
-    def test_8(self):
-        """Testar um digrafo com aresta dupla."""
-        self.assertEqual(contar_arestas([
-            [0, 1, 0],
-            [1, 0, 0],
-            [0, 0, 0]
-        ],valorado=False, digrafo=True), 2)
-
-    def test_9(self):
-        """Testar um digrafo com múltiplas arestas."""
-        self.assertEqual(contar_arestas([
-            [0, 1, 0, 0],
-            [1, 0, 1, 0],
-            [0, 1, 0, 2],
-            [0, 0, 2, 0]
-        ],valorado=False, digrafo=True), 8)
-
-    def test_10(self):
-        """Testar um digrafo com laço e arestas duplas."""
-        self.assertEqual(contar_arestas([
-            [0, 0, 0],
-            [0, 1, 1],
-            [2, 1, 0]
-        ],valorado=False, digrafo=True), 5)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
